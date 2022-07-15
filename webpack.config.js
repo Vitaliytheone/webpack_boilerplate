0a7eeca8122867da70832c242315bc45b8603692
Vitaliy <Vitaliythesecond@gmail.com>
2022-07-16 01:51:59 +0300

    styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 let mode = "development";
 if (process.env.NODE_ENV === "production") {
@@ -9,6 +10,9 @@ if (process.env.NODE_ENV === "production") {
 const plugins = [
     new HtmlWebpackPlugin({
         template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
     }),
 ];
 
@@ -29,6 +33,12 @@ module.exports = {
     },
 
     module: {
-        rules: [{ test: /\.(html)$/, use: ["html-loader"] }],
+        rules: [
+            { test: /\.(html)$/, use: ["html-loader"] },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
+        ],
     },
 };
