0947a99febe45e1080d675cce884fa9b96caaf1d
Vitaliy <Vitaliythesecond@gmail.com>
2022-07-16 01:15:49 +0300

    assets

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "assets/[hash][ext][query]",
         clean: true,
     },
     devtool: "source-map",
