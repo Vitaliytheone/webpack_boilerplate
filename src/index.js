161ed015b08d700cd5fb3d50f61e6105c0bc156a
Vitaliy <Vitaliythesecond@gmail.com>
2022-07-22 22:26:52 +0300

    react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const sayHello = () => console.log("hello");
-sayHello();
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
